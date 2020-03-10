bcf2af0f5ae13927e0f2c884fa83e48fc44579f6
DariuszLiszka <dariusz.liszka96@gmail.com>
2020-03-10 20:35:24 +0100

    slider styling

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './slider.module.css';
 // import Slide from './Slide';
 class Slider extends React.Component {
+  state = { index: 1 };
   images = {
     src: [
       'https://static-cdn.123rf.com/images/v5/index-thumbnail/84170952-b.jpg',
@@ -15,8 +16,16 @@ class Slider extends React.Component {
     return (
       <div>
         <div className={styles.sliderContainer}>
-          {this.images.src.map(el => (
-            <img clssName={styles.slide} src={el} />
+          {this.images.src.map((el, index) => (
+            <img
+              className={styles.slide}
+              src={el}
+              style={
+                this.state.index == index
+                  ? { transform: 'scale(1.1)' }
+                  : { transform: 'scale(0.9' }
+              }
+            />
           ))}
         </div>
         <button>prev slide</button>
