f71b09d61ae21af9eb9c1e8a73768f6aba1425b1
DariuszLiszka <dariusz.liszka96@gmail.com>
2020-03-12 13:27:39 +0100

    some changes

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './slider.module.css';
 // import Slide from './Slide';
 class Slider extends React.Component {
-  state = { index: 2 };
-  images = {
-    src: [
+  state = {
+    index: 2,
+    images: [
       'https://static-cdn.123rf.com/images/v5/index-thumbnail/84170952-b.jpg',
       'https://dailyweb.pl/wp-content/uploads/2019/02/500px-1200x1200.jpg',
       'https://cdn-dcp.avt.pl/i/images/2/6/9/_src_18269-500px.jpg',
@@ -12,17 +12,29 @@ class Slider extends React.Component {
       'https://i.wpimg.pl/730x0/m.fotoblogia.pl/karcz-6cb7d7a110be2853fe8504b0cb.jpg'
     ]
   };
+
   prevSlide = () => {
-    this.setState({ index: this.state.index - 1 });
+    const images = [...this.state.images];
+    const popped = images.pop();
+    images.unshift(popped);
+
+    this.setState({ images });
   };
   nextSlide = () => {
-    this.setState({ index: this.state.index + 1 });
+    const images = [...this.state.images];
+    const first = images.shift();
+    images.push(first);
+
+    this.setState({ images });
   };
   render() {
+    {
+      console.log(this.images);
+    }
     return (
       <div>
         <div className={styles.sliderContainer}>
-          {this.images.src.map((el, index) => (
+          {this.state.images.map((el, index) => (
             <img
               className={styles.slide}
               src={el}
@@ -32,27 +44,30 @@ class Slider extends React.Component {
                       position: 'absolute',
                       left: `${600}px`,
                       transform: 'scale(1.5)',
-                      'z-index': '1000'
+                      'z-index': '1000',
+                      transition: ' all 2s'
                     }
-                  : { transform: 'scale(0.9)' }
+                  : { filter: 'blur(2px)' }
               }
             />
           ))}
         </div>
-        <button
-          onClick={() => {
-            this.prevSlide();
-          }}
-        >
-          prev slide
-        </button>
-        <button
-          onClick={() => {
-            this.nextSlide();
-          }}
-        >
-          next slide
-        </button>
+        <div className={styles.buttons}>
+          <button
+            onClick={() => {
+              this.prevSlide();
+            }}
+          >
+            prev slide
+          </button>
+          <button
+            onClick={() => {
+              this.nextSlide();
+            }}
+          >
+            next slide
+          </button>
+        </div>
       </div>
     );
   }
